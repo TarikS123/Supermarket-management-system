ad46a780527b3c963026b52e8f40ac5bd9db0bd1
Tarik Sefer <nekoo1255@gmail.com>
2024-01-27 18:01:54 +0100

    Project initalization

---
+++ Supermarket MS/Supermarket MS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MySql.Data;
+namespace SupermarketMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
