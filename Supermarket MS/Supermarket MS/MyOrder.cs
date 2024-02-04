8f44c5b6cbc144857b7bfdc24e4dec6add391abf
Tarik Sefer <nekoo1255@gmail.com>
2024-02-04 23:49:19 +0100

    implemented buy function for customers. now they can place orders and we can keep track of the orders via the singleton

---
+++ Supermarket MS/Supermarket MS/MyOrder.cs
@@ -0,0 +1,59 @@
+﻿namespace SupermarketMS
+{
+    public class MyOrder  // klasa koja omogucava kupcu da pravi narudzbu
+    {
+        private static int currid=0;
+        public int OrderID { get;  }
+        public DateTime OrderDate { get; set; }
+        public Customer Customer { get; set; }
+        public double TotalPrice { get; set; }
+        public string Status { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        
+        public List<OrderItem> items {  get; set; }
+       
+
+        
+        public MyOrder(Customer customer, string status, string deliveryAddress, DateTime? deliveryDate)
+        {
+            OrderID = currid++;
+            OrderDate = DateTime.Now;
+            Customer = customer;
+            Status = status;
+            DeliveryAddress = deliveryAddress;
+            DeliveryDate = deliveryDate;
+            items = new List<OrderItem>();
+            
+        }
+
+        public void AddItem (Product item, double quantity)
+        {
+            items.Add(new OrderItem(item, quantity));
+
+        }
+
+        public void CalculateTotal() // izracuna koliko je ovaj proizvod generisao prodaje 
+        {
+            double total = 0;
+            foreach(OrderItem item in items) { total += item.product.price * item.quantity; }
+            this.TotalPrice = total;
+            
+        }
+    }
+
+    public class OrderItem // sluzi samo da bi mogao napraviti 1:many vezu tj da kupac moze kupiti vise jednog proizvoda a da ga ne cuvamo vise puta.
+    {
+        public Product product { get; set; }
+        public double quantity { get; set; }
+
+        public OrderItem(Product product, double quantity)
+        {
+
+            this.product = product;
+            this.quantity = quantity;
+        }
+    }
+
+    
+}
---
+++ Supermarket MS/Supermarket MS/OrderSingletone.cs
@@ -0,0 +1,50 @@
+﻿using Mysqlx.Crud;
+
+namespace SupermarketMS
+{
+    public class OrderSingleton   // klasa koja vodi evidencije o narudzbama. vec smo napravili da kupac moze napraviti narudzbu.
+    {                             // ova klasa omogucuje da kupac moze napraviti vise narudzbi i da pri tome ostavi trag
+        private static OrderSingleton instance;
+        private List<MyOrder> orders;
+
+        private OrderSingleton()
+        {
+            orders = new List<MyOrder>();
+        }
+
+        public static OrderSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new OrderSingleton();
+                }
+                return instance;
+            }
+        }
+
+        internal void AddOrder(MyOrder order)
+        {
+            orders.Add(order);
+        }
+
+        public List<MyOrder> GetAllOrders()
+        {
+            return orders;
+        }
+
+        public MyOrder FindOrderById(int orderId)
+        {
+            foreach (MyOrder order in orders)
+            {
+                if (order.OrderID == orderId)
+                {
+                    return order;
+                }
+            }
+            return null; 
+        }
+    }
+
+}
