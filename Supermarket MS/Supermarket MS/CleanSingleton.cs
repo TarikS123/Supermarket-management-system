a46d8913ba74266a952b7d9ccf1799f7362ca2bd
Tarik Sefer <nekoo1255@gmail.com>
2024-02-04 23:51:44 +0100

    added a class that represents the jobs that need to be done regarding cleaning, this is just to represent how the project could be improved by adding jobes for all posible employees

---
+++ Supermarket MS/Supermarket MS/CleanSingleton.cs
@@ -0,0 +1,50 @@
+﻿using SupermarketMS;
+
+namespace SupermarketMS
+{
+    public class CleanJobSingleton  // sluzi da se moze voditi evidencija o poslovima za higijenicare tj koji department treba ocistiti
+    {
+        private static CleanJobSingleton instance;
+        private List<DirtyDepartment> dirtyDepartments;
+        public CleanJobSingleton()
+        {
+            dirtyDepartments = new List<DirtyDepartment> ();
+        }
+        public static CleanJobSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CleanJobSingleton();
+                }
+                return instance;
+            }
+
+        }
+
+        public void AddCleaningJob(DirtyDepartment department)
+        {
+            dirtyDepartments.Add (department);  
+        }
+        public void CleanDepartment(string department)
+        {
+            foreach(DirtyDepartment d in dirtyDepartments)
+            {
+                if (d.Department.Equals(department)) dirtyDepartments.Remove(d);
+            }
+
+        }
+    }
+
+    public class DirtyDepartment
+    {
+        public string Department {  get; set; }
+        public string CleaningDescription {  get; set; }
+
+        public DirtyDepartment(string dep, string desc) {
+        Department = dep;
+        CleaningDescription = desc;
+        }
+    }
+}
