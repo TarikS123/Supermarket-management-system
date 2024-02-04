699ee8f321d11c0f5974d8cfbfc4582756b31eb5
Tarik Sefer <nekoo1255@gmail.com>
2024-02-04 23:41:46 +0100

    created classes that represent employees and a singleton class to keep track of employees and everything related to them. inheritance used for employee jobs

---
+++ Supermarket MS/Supermarket MS/Employee.cs
@@ -0,0 +1,133 @@
+﻿namespace SupermarketMS
+{
+    public class Employee
+    {
+       
+            public int EmployeeID { get; }
+            private static int currid = 0;
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            private string Email { get; set; }
+            private string PhoneNumber { get; set; }
+            private string Address { get; set; }
+            private double Salary { get; set; }
+            public DateTime HireDate { get; set; }
+            public string Role { get; set; }
+            
+            public string Department {  get; set; }
+
+            
+            public Employee(string firstName, string lastName, string email, string phoneNumber, string address, double salary, DateTime hireDate, string role, string department)
+            {
+                EmployeeID = currid++;
+                FirstName = firstName;
+                LastName = lastName;
+                Email = email;
+                PhoneNumber = phoneNumber;
+                Address = address;
+                Salary = salary;
+                HireDate = hireDate;
+                Role = role;
+                Department = department;
+                
+            }
+    }
+
+    public class Manager : Employee  // predstavlja menadzera koji moze zaposljavati, davati otkaz, davati zadatke i voditi evidenciju o uposlenicima
+    {
+
+       
+        public Manager( string firstName, string lastName, string email, string phoneNumber, string address, double salary, DateTime hireDate, string department)
+            : base(firstName, lastName, email, phoneNumber, address, salary, hireDate, $"Manager of {department}", department)
+        {
+            Department = department;
+        }
+
+        public void AddCleaningTask(string CleaningDescription)
+        {
+            CleanJobSingleton.Instance.AddCleaningJob(new DirtyDepartment(this.Department, CleaningDescription)); // menadzer vodi evidenciju o cistoci svom deparmentu
+        }
+
+        public void Hire(Employee emp)
+        {
+            EmployeeSingleton.Instance.AddEmployee(emp);
+        }
+
+        public void FireByNameAndSurname(string name, string surname)
+        {
+            EmployeeSingleton.Instance.RemoveEmployeeByNameAndSurname(name,surname);
+        }
+        public void FireById (int id)
+        {
+            EmployeeSingleton.Instance.RemoveEmployeeById(id);
+        }
+
+        public void GetAllEmployees()
+        {
+            EmployeeSingleton.Instance.GetAllEmployees();
+        }
+
+        public List<Employee> GetEmployeesFromParticularDepartment(string department)
+        {
+            List<Employee> returnList= new List<Employee>();
+            List<Employee> employees = EmployeeSingleton.Instance.GetAllEmployees();
+            foreach (Employee employee in employees)
+            {
+                if (employee.Department.Equals(department))returnList.Add(employee);
+            }
+            return returnList;
+        }
+
+        public List<Employee> GetEmployeesFromManagerDepartment(string department)
+        {
+          return  GetEmployeesFromParticularDepartment(this.Department);
+        }
+    }
+
+    public class Cashier : Employee
+    {
+        
+        private int CashRegisterNo {  get; set; }
+
+        public Cashier(string firstName, string lastName, string email, string phoneNumber, string address, double salary, DateTime hireDate, string department, int cashRegisterNo)
+            : base(firstName, lastName, email, phoneNumber, address, salary, hireDate, $"Cashier working on {cashRegisterNo} cash register", department)
+        {
+            
+            CashRegisterNo = cashRegisterNo;
+        }
+    }
+
+
+    public class Replenisher : Employee   // predstavlja radnike koji pune police 
+    {
+       
+        
+        public Replenisher(string firstName, string lastName, string email, string phoneNumber, string address, double salary, DateTime hireDate, string department)
+            : base(firstName, lastName, email, phoneNumber, address, salary, hireDate, $"Replenisher of {department}", department)
+        {
+            Department = department;
+            
+        }
+
+        public void Replenish() { }   // nisam napravio singleton koji predstavlja police, ova metoda bi samo dodala kvantitet na police
+
+    }
+
+    public class Cleaner : Employee   
+    {
+
+        public int jobsDone;
+
+        public Cleaner(string firstName, string lastName, string email, string phoneNumber, string address, double salary, DateTime hireDate, string department)
+            : base(firstName, lastName, email, phoneNumber, address, salary, hireDate, $"Cleaner of {department}", department)
+        {
+            jobsDone = 0; // vodi evidenciju o koliko je uposlenik posla uradio
+        }
+
+        public void Clean() { CleanJobSingleton.Instance.CleanDepartment(this.Department); jobsDone++; }   // garantuje da higijenicari odrzavaju samo svoje departmente
+
+    }
+
+
+
+}
---
+++ Supermarket MS/Supermarket MS/EmployeeSingleton.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ManagerClass")]
+
+namespace SupermarketMS
+{
+    public class EmployeeSingleton   // klasa koju koristimo za cuvanje podataka o uposlenicima na jednom mjestu
+    {
+        private static EmployeeSingleton instance;
+        private List<Employee> employees;
+
+        private EmployeeSingleton()
+        {
+            employees = new List<Employee>();
+            
+        }
+
+        public static EmployeeSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EmployeeSingleton();
+                }
+                return instance;
+            }
+        }
+
+        internal Employee GetEmployeebyNameAndSurname(string name, string surname)
+        {   
+            Employee returnEmployee = null;
+            foreach (Employee employee in employees)
+            {
+                if (employee.FirstName == name && employee.LastName.Equals(surname))
+                    returnEmployee=employee;
+            }
+            return returnEmployee;
+        }
+         // obzirom da cuvam uposlenike sortirane po id-u, iskoristio sam binary searhch za njihovo brzo trazenje po id-u
+        internal Employee GetEmployeeById(int employeeId)
+        {
+            
+            int low = 0;
+            int high = employees.Count - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                Employee midEmployee = employees[mid];
+
+                if (midEmployee.EmployeeID == employeeId)
+                {
+                    return midEmployee; 
+                }
+                else if (midEmployee.EmployeeID < employeeId)
+                {
+                    low = mid + 1; 
+                }
+                else
+                {
+                    high = mid - 1; 
+                }
+            }
+
+            return null; 
+        }
+
+        internal void RemoveEmployeeById(int employeeId)  
+        {
+            RemoveEmployee(GetEmployeeById(employeeId)); // iskoristio funkciju opet 
+        }
+
+        internal void RemoveEmployeeByNameAndSurname(string employeeName, string surname)
+        {
+            RemoveEmployee(GetEmployeebyNameAndSurname(employeeName, surname));
+        }
+
+        internal void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+        private void RemoveEmployee(Employee employee)
+        {
+            employees.Remove(employee);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
+    }
+
+}
